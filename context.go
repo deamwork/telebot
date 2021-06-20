06218c96c94c1e8791674da36de5da906921cbbb
Jason Cooper <master@deamwork.com>
2021-06-21 01:17:43 +0800

    impl context.Context for telebot.Context

--- context.go
+++ context.go
@@ -3,6 +3,7 @@ package telebot
 import (
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -131,6 +132,33 @@ type Context interface {
 
 	// Set saves data in the context.
 	Set(key string, val interface{})
+
+	// Impl of native net/context.Context
+
+	// Deadline returns the time when work done on behalf of this context
+	// should be canceled. Deadline returns ok==false when no deadline is
+	// set. Successive calls to Deadline return the same results.
+	Deadline() (deadline time.Time, ok bool)
+
+	// Done returns a channel that's closed when work done on behalf of this
+	// context should be canceled. Done may return nil if this context can
+	// never be canceled. Successive calls to Done return the same value.
+	// The close of the Done channel may happen asynchronously,
+	// after the cancel function returns.
+	Done() <-chan struct{}
+
+	// Err
+	// If Done is not yet closed, Err returns nil.
+	// If Done is closed, Err returns a non-nil error explaining why:
+	// Canceled if the context was canceled
+	// or DeadlineExceeded if the context's deadline passed.
+	// After Err returns a non-nil error, successive calls to Err return the same error.
+	Err() error
+
+	// Value returns the value associated with this context for key, or nil
+	// if no value is associated with key. Successive calls to Value with
+	// the same key returns the same result.
+	Value(key interface{}) interface{}
 }
 
 // nativeContext is a native implementation of the Context interface.
@@ -385,4 +413,19 @@ func (c *nativeContext) Get(key string) interface{} {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 	return c.store[key]
+}
+
+func (c *nativeContext) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (c *nativeContext) Done() <-chan struct{} {
+	return nil
+}
+
+func (c *nativeContext) Err() error {
+	return nil
+}
+func (c *nativeContext) Value(key interface{}) interface{} {
+	return nil
 }
